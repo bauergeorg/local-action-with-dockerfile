bd5fe14443f3ef5c897db7c9e29ec56249576de1
Georg Bauer <bauer@rohmann.de>
2021-02-15 08:43:08 +0100

    add stuff

---
+++ special_functions.py
@@ -0,0 +1,2 @@
+def my_favorite_math_function(var):
+    return int(var) * 2
---
+++ test.py
@@ -0,0 +1,7 @@
+from special_functions import *
+
+testVar = [0, 2, 4, 6, 8, 9]
+
+for i in testVar: 
+    x = my_favorite_math_function(str(i))
+    print("Input: " + str(i) + " | Result: " + str(x))
