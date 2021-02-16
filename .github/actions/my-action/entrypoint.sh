e02e00e47f815cc908ec482c5488f756789a0031
Georg Bauer <bauer@rohmann.de>
2021-02-16 14:24:35 +0100

    change test

--- .github/actions/my-action/entrypoint.sh
+++ .github/actions/my-action/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
-echo "dadkjandskjansdkjnkasjdnkjasndkjansdk"
+echo "--------- Print Version ---------"
+python3 --version
+cat /etc/os-release
+echo "--------- Run Test ---------"
+python3 test.py
